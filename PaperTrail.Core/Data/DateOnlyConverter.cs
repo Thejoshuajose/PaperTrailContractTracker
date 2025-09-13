f0b6af4c25baf45b76702818b994d07290850a1e
Joshua Gonzales <122904252+Thejoshuajose@users.noreply.github.com>
2025-09-13 05:16:04 -0400

    Fix DateOnly conversion setup

---
+++ PaperTrail.Core/Data/DateOnlyConverter.cs
@@ -0,0 +1,12 @@
+using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
+
+namespace PaperTrail.Core.Data;
+
+public class DateOnlyConverter : ValueConverter<DateOnly, DateTime>
+{
+    public DateOnlyConverter()
+        : base(d => d.ToDateTime(TimeOnly.MinValue), d => DateOnly.FromDateTime(d))
+    {
+    }
+}
+
