c0da7ca52bbef79f9411a26cf9642edc42d4232f
FIWB Solutions <fiwbsolutions@outlook.com>
2025-09-13 15:10:48 -0400

    past commit broke the build

--- PaperTrail.App/obj/Debug/net8.0-windows/PaperTrail.App.AssemblyInfo.cs
+++ PaperTrail.App/obj/Debug/net8.0-windows/PaperTrail.App.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PaperTrail.App")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d8cc2bfb20146e1e3f8b0515aa48561b4e4c41a4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+530adcfcef2e6472db38c3754293830b585fa26d")]
 [assembly: System.Reflection.AssemblyProductAttribute("PaperTrail.App")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PaperTrail.App")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
