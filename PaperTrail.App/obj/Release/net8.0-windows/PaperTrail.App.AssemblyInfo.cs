1ba870eb8aec58751ee2ddca64794c65db0d1191
FIWB Solutions <fiwbsolutions@outlook.com>
2025-09-13 05:35:54 -0400

    changes

--- PaperTrail.App/obj/Release/net8.0-windows/PaperTrail.App.AssemblyInfo.cs
+++ PaperTrail.App/obj/Release/net8.0-windows/PaperTrail.App.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PaperTrail.App")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9799afbdf9a65a1ec4316759bd8a051e7edaf09a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+344fbe555acd89dd6c66b88a395a7d25dbbccdf6")]
 [assembly: System.Reflection.AssemblyProductAttribute("PaperTrail.App")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PaperTrail.App")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
