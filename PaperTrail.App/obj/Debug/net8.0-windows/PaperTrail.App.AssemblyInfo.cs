a65ef5d20606ad4a8fe997bbb45ce84992284d48
FIWB Solutions <fiwbsolutions@outlook.com>
2025-09-14 00:52:28 -0400

    sync

--- PaperTrail.App/obj/Debug/net8.0-windows/PaperTrail.App.AssemblyInfo.cs
+++ PaperTrail.App/obj/Debug/net8.0-windows/PaperTrail.App.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PaperTrail.App")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f5d60dfca0622d2b57137c9cc9e24d6d52b5752e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ea1913c960da69785a6505afa778499b3ddc2fe6")]
 [assembly: System.Reflection.AssemblyProductAttribute("PaperTrail.App")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PaperTrail.App")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
